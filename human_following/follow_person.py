a556e468000748ec630c0c0b3b27d7ea918ff09f
Haruki <haruki.isono861@gmail.com>
2025-02-10 18:59:42 +0900

    fix

--- human_following/follow_person.py
+++ human_following/follow_person.py
@@ -3,29 +3,39 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, Point
+from std_msgs.msg import Bool
 
 class FollowPersonNode(Node):
     def __init__(self):
         super().__init__('follow_person_node')
         self.subscription = self.create_subscription(Point, '/human_position', self.position_callback, 10)
-        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.publisher_cmd = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.publisher_bool = self.create_publisher(Bool, '/too_close', 10)
 
         self.target_x = 320
         self.kp_angular = 0.005
         self.kp_linear = 0.5
         self.min_distance = 0.6
+        self.stop_distance = 1.0
 
     def position_callback(self, msg):
         cx, cy, distance = msg.x, msg.y, msg.z
         cmd = Twist()
+        too_close_msg = Bool()
 
-        error_x = cx - self.target_x
-        cmd.angular.z = -self.kp_angular * error_x
-
-        if distance > self.min_distance:
+        if distance > self.stop_distance:
+            error_x = cx - self.target_x
+            cmd.angular.z = -self.kp_angular * error_x
             cmd.linear.x = min(self.kp_linear * (distance - self.min_distance), 0.5)
+            too_close_msg.data = False
+        else:
+            cmd.linear.x = 0.0
+            cmd.angular.z = 0.0
+            too_close_msg.data = True
 
-        self.publisher.publish(cmd)
+        self.publisher_cmd.publish(cmd)
+        self.publisher_bool.publish(too_close_msg)
+
 
 def main():
     rclpy.init()
@@ -33,6 +43,7 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
