547509ca44f7c4a2f46fd200f1b8eab06966ea3d
Haruki <haruki.isono861@gmail.com>
2025-02-10 19:17:20 +0900

    fix: 1m

--- human_following/follow_person.py
+++ human_following/follow_person.py
@@ -13,15 +13,22 @@ class FollowPersonNode(Node):
         self.publisher_bool = self.create_publisher(Bool, '/too_close', 10)
 
         self.target_x = 320
-        self.kp_angular = 0.005
+        self.kp_angular = 0.01
         self.kp_linear = 0.5
         self.min_distance = 0.6
         self.stop_distance = 1.0
+
+        self.tracking_id = None  # 最初に検出した人のID
 
     def position_callback(self, msg):
         cx, cy, distance = msg.x, msg.y, msg.z
         cmd = Twist()
         too_close_msg = Bool()
+
+        if self.tracking_id is None:
+            self.tracking_id = (cx, cy)  # 最初に検出した人を記録
+        elif abs(cx - self.tracking_id[0]) > 50 or abs(cy - self.tracking_id[1]) > 50:
+            return  # 別の人と判断し無視
 
         if distance > self.stop_distance:
             error_x = cx - self.target_x
